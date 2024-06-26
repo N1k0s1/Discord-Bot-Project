f1b71c9cffc06b6e1d582c98fbc295d25f52ad35
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-06-26 21:41:33 +1200

    Did a lot of debugging in main, commands not working and syncing, still working on it
    Did a lot of debugging in main, commands not working and syncing, still working on it

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import discord
+import json
 from discord.ext import commands
+from discord import app_commands
+import logging
 
+TOKEN = '' 
 intents = discord.Intents.default()
-intents.members = True
+intents.members = True 
+intents.message_content = True  
+intents.presences = True 
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', case_insensitive=True, intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+
+tree = bot._connection._command_tree 
 
 # Load cogs
-initial_extensions = ['usermanagement.UserManagement']
+initial_extensions = ['cogs.AutomatedMessaging', 'cogs.ChannelManagement', 'cogs.CoachingSessionCheckIn', 'cogs.CohortManagement', 'cogs.FeedbackCollection', 'cogs.LearnerNudges', 'cogs.SuggestionCollection', 'cogs.SupportTicket', 'cogs.UserManagement', 'cogs.WeeklyCheckIn', 'cogs.Greetings']
 
 if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)
 
-bot.run('bottoken')
+@bot.command(name='sync', description='Owner only')
+async def sync(interaction: discord.Interaction):
+        print('Command tree synced.')
+        await tree.sync(guild=discord.Object(id=1255437522629169285))
+        print('Command tree synced.')
+
+@bot.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1255437522629169285))
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    await bot.change_presence(activity=discord.Activity(name='Working on REA', type=0, url='https://rea.coach'))
+    print(f'Successfully logged in and booted...!')
+
+
+
+bot.run(TOKEN) 
