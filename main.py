cd9548551ae60dc84a3933fb3847d9c9b0b5cd42
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-07-01 13:43:59 +1200

    Updated the main.py file to add in cogs, and made a basic cog, for proof of concept.
    Updated the main.py file to add in cogs, and made a basic cog, for proof of concept.
    Now I can work on more cogs as they're working, with pycord

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 import discord
-import json
-from discord.ext import commands
-from discord import app_commands
-import logging
 
-TOKEN = '' 
-intents = discord.Intents.default()
-intents.members = True 
-intents.message_content = True  
-intents.presences = True 
-
-bot = commands.Bot(command_prefix='/', case_insensitive=True, intents=intents)
-
-
-tree = bot._connection._command_tree 
-
-# Load cogs
-initial_extensions = ['cogs.AutomatedMessaging', 'cogs.ChannelManagement', 'cogs.CoachingSessionCheckIn', 'cogs.CohortManagement', 'cogs.FeedbackCollection', 'cogs.LearnerNudges', 'cogs.SuggestionCollection', 'cogs.SupportTicket', 'cogs.UserManagement', 'cogs.WeeklyCheckIn', 'cogs.Greetings']
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        bot.load_extension(extension)
-
-@bot.command(name='sync', description='Owner only')
-async def sync(interaction: discord.Interaction):
-        print('Command tree synced.')
-        await tree.sync(guild=discord.Object(id=1255437522629169285))
-        print('Command tree synced.')
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=1255437522629169285))
-    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    await bot.change_presence(activity=discord.Activity(name='Working on REA', type=0, url='https://rea.coach'))
-    print(f'Successfully logged in and booted...!')
+    print(f"We have logged in as {bot.user}")
 
 
 
-bot.run(TOKEN) 
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def cogs(ctx, cogs):
+    if cogs in cogs:
+        bot.load_extension(f'cogs.{cogs}')
+        await ctx.send(f"Successfully loaded {cogs}")
+
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def ping(ctx):
+    await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def sync(ctx): 
+    await bot.sync_commands()
+    await ctx.send(f"succesfully synced commands")
+
+@bot.slash_command(guild_ids=[1255437522629169285])
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+
+bot.run('tokenhere:)')
