f143292a635f59bbf9d148a0a899a8e08540d065
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-08-01 09:40:36 +1200

    trying to get channelmangement wokring
    was working with only one user, but i need to be able to input multiple users, added a delete command that is admin/channel creator only

--- cogs/ChannelManagement.py
+++ cogs/ChannelManagement.py
@@ -1,21 +1,27 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 
 class ChannelManagement(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
-@commands.slash_command(name="createchannel", description="Create a new channel with specified users having access", guild_ids=[1255437522629169285])
-async def createchannel(ctx, channelname: str, users: discord.Member):
-    guild = ctx.guild
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        guild.me: discord.PermissionOverwrite(read_messages=True)
-    }
-    overwrites[users] = discord.PermissionOverwrite(read_messages=True)
-    await guild.create_text_channel(channelname, overwrites=overwrites)
-    await ctx.respond("You have created a new channel!")
-
+    @commands.slash_command(name="createchannel", description="Create a new channel with specified users having access", guild_id=id)
+    async def createchannel(self, ctx, channelname: str, users: discord.Member):
+        guild = ctx.guild
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True)
+        }
+        overwrites[users] = discord.PermissionOverwrite(read_messages=True)
+        await guild.create_text_channel(channelname, overwrites=overwrites)
+        await ctx.respond("You have created a new channel!")
+        
+    @commands.slash_command(name="deletechannel", description="Delete a channel", guild_id=id)
+    async def deletechannel(self, ctx, channel: discord.TextChannel):
+        if ctx.author == channel.creator or ctx.author.guild_permissions.administrator:
+            await channel.delete()
+            await ctx.respond("Channel deleted successfully!")
+        else:
+            await ctx.respond("You do not have permission to delete this channel.")
 def setup(bot):
     bot.add_cog(ChannelManagement(bot))
