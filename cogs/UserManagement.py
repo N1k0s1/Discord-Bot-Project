57a4ae0c3a412fdc94752f84f6cd430ef7add654
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-08-21 11:38:16 +1200

    working on role assignements
    working on role assignments, trying to make a drop down box that works.

--- cogs/UserManagement.py
+++ cogs/UserManagement.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ui import Select
 import asyncio
 
 id = [1255437522629169285]
@@ -9,23 +10,41 @@ class UserManagement(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(name="rolesetup", description="Set your cohort and location roles", guild_ids=id)
-    async def roletest(self, ctx, member: discord.Member, cohort: str):
-        role_id = None
-        if cohort == "cohort 1":
-            role_id = 1258531898427314237
-        elif cohort == "cohort 2":
-            role_id = 1258531898427314238
-        else:
-            await ctx.respond("Invalid cohort")
-            return
+    async def roletest(self, ctx, member: discord.Member):
+        options = [
+            discord.SelectOption(label="Cohort 1", value="cohort_1"),
+            discord.SelectOption(label="Cohort 2", value="cohort_2")
+        ]
+        select = Select(options=options, placeholder="Select your cohort", min_values=1, max_values=1)
 
-        guild = ctx.guild
-        role = discord.utils.get(guild.roles, id=role_id)
-        if role is not None:
-            await member.add_roles(role)
-            await ctx.respond("Role found")
-        else:
-            await ctx.respond("Role not found")
+        await ctx.send("Please select your cohort:", view=select)
+
+        def check(interaction):
+            return interaction.user == ctx.author and interaction.message == ctx.message
+
+        try:
+            interaction = await self.bot.wait_for("select_option", check=check, timeout=60)
+            cohort = interaction.values[0]
+
+            role_id = None
+            if cohort == "cohort_1":
+                role_id = 1258531898427314237
+            elif cohort == "cohort_2":
+                role_id = 1258531898427314238
+            else:
+                await ctx.respond("Invalid cohort")
+                return
+
+            guild = ctx.guild
+            role = discord.utils.get(guild.roles, id=role_id)
+            if role is not None:
+                await member.add_roles(role)
+                await ctx.respond("Role found")
+            else:
+                await ctx.respond("Role not found")
+
+        except asyncio.TimeoutError:
+            await ctx.respond("Selection timed out.")
 
 def setup(bot):
     bot.add_cog(UserManagement(bot))
